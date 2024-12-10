ab63a395a5d7002421222accd6057be3638a5b9c
Saan :b <134438179+saaantinooo@users.noreply.github.com>
2024-12-10 10:53:51 -0300

    Add files via upload

---
+++ landing page/cats.js
@@ -0,0 +1,7 @@
+function mostrarFormulario() {
+    document.getElementById("formulario-modal").classList.remove("hidden");
+}
+
+function ocultarFormulario() {
+    document.getElementById("formulario-modal").classList.add("hidden");
+}
