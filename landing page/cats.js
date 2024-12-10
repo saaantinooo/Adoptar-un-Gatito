28373b8c80c3163373b1038e095ff76b5e6ecafa
Saan :b <134438179+saaantinooo@users.noreply.github.com>
2024-12-10 10:55:42 -0300

    Delete landing page directory

--- landing page/cats.js
+++
@@ -1,7 +0,0 @@
-function mostrarFormulario() {
-    document.getElementById("formulario-modal").classList.remove("hidden");
-}
-
-function ocultarFormulario() {
-    document.getElementById("formulario-modal").classList.add("hidden");
-}
